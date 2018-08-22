96b432b37c221f64ceafbed6088f5467d2818c0a
S <slimeball8899@gmail.com>
2018-08-22 14:23:03 +1200

    Add dummy user.

--- app/src/main/java/com/example/nickyhe/foodapp/LoginActivity.java
+++ app/src/main/java/com/example/nickyhe/foodapp/LoginActivity.java
@@ -49,7 +49,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "foo@example.com:hello", "bar@example.com:world", "Test@test.com:Test"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
