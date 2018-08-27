018b3f8b18c7018089115f99ba79fbac2a67967b
NickyHe <nickyhe123@gmail.com>
2018-08-27 18:26:18 +1200

    adding login activity and signup activity

--- app/src/main/java/com/nickhe/reciperescue/MainActivity.java
+++ app/src/main/java/com/nickhe/reciperescue/MainActivity.java
@@ -1,10 +1,25 @@
 package com.nickhe.reciperescue;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void login(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+
+        startActivity(intent);
+    }
+
+    public void signup(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
