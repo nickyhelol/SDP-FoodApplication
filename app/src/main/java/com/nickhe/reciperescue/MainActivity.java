71c3718586355ef50471d05ed283d62de0225d87
William Anderson <slimeball8899@gmail.com>
2018-08-28 13:08:29 +1200

    Modifies login activity. Adds shared preference template for login session. Adds navigation drawer template.

--- app/src/main/java/com/nickhe/reciperescue/MainActivity.java
+++ app/src/main/java/com/nickhe/reciperescue/MainActivity.java
@@ -1,24 +1,26 @@
 package com.nickhe.reciperescue;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String accountPref = "accountPref";
+    public static String emailPref = "emailPref";
+    public static String passPref = "passPref";
+    public static SharedPreferences sharedPreferences;
+
     public void login(View view)
     {
-        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-
-        startActivity(intent);
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
     }
 
     public void signup(View view)
     {
-        Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
-
-        startActivity(intent);
+        startActivity(new Intent(getApplicationContext(), SignupActivity.class));
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
