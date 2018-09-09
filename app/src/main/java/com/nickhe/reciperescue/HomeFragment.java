aa9be63a6d40355965e7100f1b03a5b313e43c91
NickyHe <nickyhe123@gmail.com>
2018-09-10 07:37:39 +1200

    adding some fragments in MainMenuActivity, establish the template

---
+++ app/src/main/java/com/nickhe/reciperescue/HomeFragment.java
@@ -0,0 +1,33 @@
+package com.nickhe.reciperescue;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
--- app/src/main/java/com/nickhe/reciperescue/MainMenuActivity.java
+++ app/src/main/java/com/nickhe/reciperescue/MainMenuActivity.java
@@ -2,36 +2,66 @@ package com.nickhe.reciperescue;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    private HomeFragment homeFragment;
+    private SearchFragment searchFragment;
+    private ProfileFragment profileFragment;
+    private RankListFragment rankListFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        homeFragment = new HomeFragment();
+        searchFragment = new SearchFragment();
+        profileFragment = new ProfileFragment();
+        rankListFragment = new RankListFragment();
+
+        setFragment(homeFragment);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.MainMenu: {
-                        // Insert main menu redirection
+                    case R.id.home_navigation: {
+                        setFragment(homeFragment);
+                        return true;
                     }
-                    case R.id.profile: {
-                        // Insert profile redirection
+                    case R.id.search_navigation: {
+                        setFragment(searchFragment);
+                        return true;
+                    }
+                    case R.id.profile_navigation: {
+                        setFragment(profileFragment);
+                        return true;
+                    }
+                    case R.id.rankList_navigation: {
+                        setFragment(rankListFragment);
+                        return true;
                     }
                 }
-                return true;
+                return false;
             }
         });
+    }
+
+    public void setFragment(Fragment fragment)
+    {
+        getSupportFragmentManager().beginTransaction().replace(R.id.contentLayout, fragment).commit();
     }
 
 }
