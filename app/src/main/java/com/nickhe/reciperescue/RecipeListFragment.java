75323f803242d24c32eb032c20d6fe277f366458
William Anderson <slimeball8899@gmail.com>
2018-09-20 22:42:20 +1200

    Adds filter by ingredients to RecipeListAdapterFilterable.java Modifies RecipeListFragment.java to show layout
    Signed-off-by: William Anderson <slimeball8899@gmail.com>

--- app/src/main/java/com/nickhe/reciperescue/RecipeListFragment.java
+++ app/src/main/java/com/nickhe/reciperescue/RecipeListFragment.java
@@ -1,6 +1,7 @@
 package com.nickhe.reciperescue;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -11,6 +12,8 @@ import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class RecipeListFragment extends Fragment {
     private RecipeListAdapterFilterable recipeListAdapterFilterable = new RecipeListAdapterFilterable(getActivity(), FakeRecipeRepository.getFakeRecipeRepository(getActivity()).getFakeRepo());
@@ -26,6 +29,8 @@ public class RecipeListFragment extends Fragment {
         Bundle searchBundle = getArguments();
         if (searchBundle.getString("Type").equals("RecipeName")) {
             recipeListAdapterFilterable.getNameFilter().filter(searchBundle.getString("Name"));
+        } else if (searchBundle.getString("Type").equals("Ingredients")) {
+            recipeListAdapterFilterable.getRecipeIngredientsFilter().filter(searchBundle.getString("Ingredients"));
         }
     }
 
@@ -39,9 +44,32 @@ public class RecipeListFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         RecyclerView recipeRecyclerView = (RecyclerView) getView().findViewById(R.id.searchRecyclerView);
+        RecyclerViewItemDecorator recyclerViewItemDecorator = new RecyclerViewItemDecorator(10);
         StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
         recipeRecyclerView.setLayoutManager(staggeredGridLayoutManager);
+        recipeRecyclerView.addItemDecoration(recyclerViewItemDecorator);
         recipeRecyclerView.setAdapter(recipeListAdapterFilterable);
+    }
+
+    private class RecyclerViewItemDecorator extends RecyclerView.ItemDecoration {
+        private int space;
+
+        public RecyclerViewItemDecorator(int space) {
+            this.space = space;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            outRect.bottom = space;
+            outRect.left = space;
+            outRect.right = space;
+
+            if (parent.getChildLayoutPosition(view) == 0 || parent.getChildLayoutPosition(view) == 1) {
+                outRect.top = space;
+            } else {
+                outRect.top = 0;
+            }
+        }
     }
 }
 //
