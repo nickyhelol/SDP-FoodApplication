34b463a6fcaf8bfa77949da9b1d6ed9175793dfd
jeson.dijamco <mms8338@autuni.ac.nz>
2018-09-22 01:44:00 +1200

    commit
    commit

--- app/src/main/java/com/nickhe/reciperescue/RecipeViewActivity.java
+++ app/src/main/java/com/nickhe/reciperescue/RecipeViewActivity.java
@@ -1,15 +1,14 @@
 package com.nickhe.reciperescue;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class RecipeViewActivity extends AppCompatActivity {
+
 
 
     @Override
@@ -17,6 +16,7 @@ public class RecipeViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_view);
 
+        //Once the intent is called, this activity picks up the recipe by its index
         int id = getIntent().getIntExtra("id", -1);
 
         Recipe recipe = null;
@@ -26,19 +26,38 @@ public class RecipeViewActivity extends AppCompatActivity {
             recipe = HomeFragment.fakeRecipeRepository.getFakeRepo().get(id);
         }
 
+        //We build the ingredients and procedure from String array back into a single string
+        StringBuilder ingredientBuilder = new StringBuilder();
+        for(String value : recipe.getRecipeIngredients()){
+
+            ingredientBuilder.append(" - " + value + "\n");
+
+        }
+
+        int count = 0;
+        StringBuilder procedureBuilder = new StringBuilder();
+        for(String value : recipe.getRecipeInstruction()){
+            count += 1;
+            procedureBuilder.append(count + ".) " + value + "\n");
+        }
+
+        //WE initialize the elements from the XML file into this activity
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        imageView.setImageBitmap(recipe.getRecipeImage());
-
         TextView recipeName = (TextView) findViewById(R.id.recipeName);
-        recipeName.setText(recipe.getRecipeTitle());
-
         TextView calorieValue = (TextView) findViewById(R.id.calorieText);
+        TextView recOwner = (TextView) findViewById(R.id.recipePublisher);
+        TextView recIngredients = (TextView) findViewById(R.id.ingredientsList);
+        TextView recProcedure = (TextView) findViewById(R.id.proceduresList);
+
+        //WE set the text/image values of the XML file that we're going to display on the layout
+        imageView.setImageBitmap(recipe.getRecipeImage());
+        recipeName.setText(recipe.getRecipeTitle());
         calorieValue.setText(recipe.getCalories());
+        recOwner.setText("Published by: " + recipe.getRecipePublisher());
+        recIngredients.setText(ingredientBuilder.toString());
+        recProcedure.setText(procedureBuilder.toString());
 
-
-
-
-       /* ArrayAdapter<String> ingredientsListViewAdapter = new ArrayAdapter<String>(
+        /* ArrayAdapter<String> ingredientsListViewAdapter = new ArrayAdapter<String>(
                 this,
                 R.layout.listview_layout,
                 R.id.ingredientNameTextView,
@@ -57,27 +76,5 @@ public class RecipeViewActivity extends AppCompatActivity {
 
         ListView proceduresListView = (ListView) findViewById(R.id.proceduresListView);
         proceduresListView.setAdapter(procedureslistViewAdapter);*/
-
-        TextView recOwner = (TextView) findViewById(R.id.recipePublisher);
-        recOwner.setText("Published by: " + recipe.getRecipePublisher());
-
-
-        StringBuilder ingredientBuilder = new StringBuilder();
-        for(String value : recipe.getRecipeIngredients()){
-
-            ingredientBuilder.append(" - " + value + "\n");
-
-        }
-        TextView recIngredients = (TextView) findViewById(R.id.ingredientsList);
-        recIngredients.setText(ingredientBuilder.toString());
-
-        int count = 0;
-        StringBuilder procedureBuilder = new StringBuilder();
-        for(String value : recipe.getRecipeInstruction()){
-            count += 1;
-            procedureBuilder.append(count + ".) " + value + "\n");
-        }
-        TextView recProcedure = (TextView) findViewById(R.id.proceduresList);
-        recProcedure.setText(procedureBuilder.toString());
     }
 }
