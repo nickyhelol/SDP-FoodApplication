4b63ef235b2dd62c693f2bf6889c54b24b620d88
jeson.dijamco <mms8338@autuni.ac.nz>
2018-09-22 01:38:34 +1200

    Committing changes
    Committing comment changes before testing

--- app/src/main/java/com/nickhe/reciperescue/RecipeViewActivity.java
+++ app/src/main/java/com/nickhe/reciperescue/RecipeViewActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
