efee26ee699ed4951c563fa02a8b26293a4fbbfc
dineshnz <31061381+dineshnz@users.noreply.github.com>
2018-09-23 21:01:45 +1200

    little change

--- app/src/main/java/com/nickhe/reciperescue/ForgotPasswordActivity.java
+++ app/src/main/java/com/nickhe/reciperescue/ForgotPasswordActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
+
 public class ForgotPasswordActivity extends Activity {
 
     private EditText emailView;
