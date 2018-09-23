f8e90c2558f68305bbd8481de94c6b2b92a9d232
dineshnz <31061381+dineshnz@users.noreply.github.com>
2018-09-23 22:41:33 +1200

    changed the testing

--- app/src/test/java/com/nickhe/reciperescue/RegisterToFirebaseActivityTest.java
+++ app/src/test/java/com/nickhe/reciperescue/RegisterToFirebaseActivityTest.java
@@ -1,5 +1,7 @@
 package com.nickhe.reciperescue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -23,4 +25,6 @@ public class RegisterToFirebaseActivityTest {
 
         assertTrue(register.isPasswordValid(password));
     }
+
+
 }
