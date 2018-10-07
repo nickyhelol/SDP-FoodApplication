637967e2095a8b39a320f916cc0c3e1880c8c7a1
William Anderson <slimeball8899@gmail.com>
2018-10-08 09:02:24 +1300

    Adds required flowlayout module.

---
+++ sample/src/main/java/com/nex3z/flowlayout/sample/MainActivity.java
@@ -0,0 +1,59 @@
+package com.nex3z.flowlayout.sample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.nex3z.flowlayout.FlowLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initButton();
+
+        fillAutoSpacingLayout();
+    }
+
+    private void initButton() {
+        Button btn = (Button) findViewById(R.id.btn_recyclerview);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RecyclerViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void fillAutoSpacingLayout() {
+        FlowLayout flowLayout = (FlowLayout) findViewById(R.id.flow);
+        String[] dummyTexts = getResources().getStringArray(R.array.lorem_ipsum);
+
+        for (String text : dummyTexts) {
+            TextView textView = buildLabel(text);
+            flowLayout.addView(textView);
+        }
+    }
+
+    private TextView buildLabel(String text) {
+        TextView textView = new TextView(this);
+        textView.setText(text);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+        textView.setPadding((int)dpToPx(16), (int)dpToPx(8), (int)dpToPx(16), (int)dpToPx(8));
+        textView.setBackgroundResource(R.drawable.label_bg);
+
+        return textView;
+    }
+
+    private float dpToPx(float dp){
+        return TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
+    }
+}
