7ca7381c83dabb96aa2c9b8eba8d8e4d72d32a46
OfficialToddHoward <42361653+OfficialToddHoward@users.noreply.github.com>
2018-10-12 01:24:58 +1300

    Google Login Features
    Added Google Login Button
    Added Google Tech
    Added Google Signin Tech
    Crashed on Actual Signin
    -Can't Test due to AMD processor-

--- app/src/main/java/com/nickhe/reciperescue/MainLoginActivity.java
+++ app/src/main/java/com/nickhe/reciperescue/MainLoginActivity.java
@@ -1,5 +1,6 @@
 package com.nickhe.reciperescue;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
@@ -7,17 +8,29 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -38,8 +51,19 @@ public class MainLoginActivity extends AppCompatActivity {
     private ProgressDialog progressDialog;
     private TextView forgotPwView;
     private TextView errorInfoView;
+    SignInButton googleSignInButton;
+    GoogleSignInClient mGoogleSignInClient;
+    private final static int RC_SIGN_IN = 9001;
+    FirebaseAuth.AuthStateListener mAuthLis;
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        firebaseAuth.addAuthStateListener(mAuthLis);
+
+    }
 
     @Override
 
@@ -53,6 +77,16 @@ public class MainLoginActivity extends AppCompatActivity {
         screenInfo.setText("No. of attempts remaining: 5");
         firebaseAuth=FirebaseAuth.getInstance();
         progressDialog= new ProgressDialog(this);
+
+        mAuthLis = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() !=null)
+                {
+                    startActivity(new Intent(MainLoginActivity.this, MainMenuActivity.class));
+                }
+            }
+        };
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
@@ -89,7 +123,76 @@ public class MainLoginActivity extends AppCompatActivity {
             }
         });
 
+        googleSignInButton = (SignInButton) findViewById(R.id.googleBtn);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+
     }
+
+
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(String.valueOf(MainLoginActivity.this), "Google sign in failed", e);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+
+
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+
+
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
 
     /**
      * This method will initialize all the variables and upon its call,
