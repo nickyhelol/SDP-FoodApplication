5dc140e38bd1fb214e352ed39878e8525818b9ce
dineshnz <31061381+dineshnz@users.noreply.github.com>
2018-10-22 17:26:49 +1300

    singleton object for database

---
+++ app/src/main/java/com/nickhe/reciperescue/UserDataManager.java
@@ -0,0 +1,35 @@
+package com.nickhe.reciperescue;
+
+import android.support.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class UserDataManager {
+
+    private static User user;
+
+    public static void retrieveUSer(FirebaseAuth firebaseAuth){
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                user = dataSnapshot.getValue(User.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public static User getUser(){
+        return user;
+    }
+}
